6b93e217131a515d358c864e042968bf256cfd21
Azim <azimuddin807@gmail.com>
2024-09-03 01:09:41 +0600

    reverse js

---
+++ concat.js
@@ -0,0 +1,7 @@
+ const frist = 'Nabid';
+ const last = 'Abid';
+
+ console.log(frist +' ' + last);
+
+ const pass = last.includes('w');
+ console.log(pass)
---
+++ reserve.js
@@ -0,0 +1,23 @@
+ const sentence = 'I am web developer';
+ let reserve = ' '
+
+ for(const letter of sentence){
+    console.log(letter);
+    reserve = letter + reserve;
+ }
+ console.log(reserve)
+
+let rev = ' ';
+
+for(i = 0; i < sentence.length; i++){
+    const latter = sentence[i];
+   rev = latter + rev;
+
+}
+console.log(rev);
+
+
+// Shortcut reserve
+
+const reserved = sentence.split('').reverse().join('');
+console.log(reserved)
