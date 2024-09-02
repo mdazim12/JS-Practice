0157494aa42b15b171426daaab0ff6d03fe6fcf3
Azim <azimuddin807@gmail.com>
2024-09-02 13:57:57 +0600

    while loop even number

---
+++ range.js
@@ -0,0 +1,19 @@
+let num = 1;
+
+while( num <= 10 ){
+    console.log(num);
+    num++;
+}
+
+// Using sum on js loop
+
+ let number = 1;
+ let sum = 0;
+
+ while (number < 10 ){
+   
+    sum = sum + number;
+    number++;
+    console.log(sum);
+ }
+
