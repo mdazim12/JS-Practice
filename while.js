8273c79f1d1a5a0e2ab5df59c45d6aa330bc1c1c
Azim <azimuddin807@gmail.com>
2024-09-02 13:32:46 +0600

    while loop

--- while.js
+++ while.js
@@ -4,3 +4,10 @@ if(number < 10 ){
     console.log('Choto akta number paise');
 }
 console.log('someting new there');
+
+let num = 0;
+
+while( num < 10 ){
+    console.log('I am looping', num);
+    num++;
+}
