f75c79bebcd94f240031be2f2147092fad9f6a53
Azim <azimuddin807@gmail.com>
2024-09-02 21:44:14 +0600

    string

---
+++ string.js
@@ -0,0 +1,11 @@
+//  const number = 2;
+//  console.log(number);
+
+//  console.log(typeof number);
+
+//  const name = 'josimuddin';
+//  console.log( name.length);
+
+ const teammate = [4,564,24,32,86,,8];
+ console.log(teammate[0]);
+ 
