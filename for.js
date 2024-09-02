ef57f5d6961cb89430c08320563018970024061d
Azim <azimuddin807@gmail.com>
2024-09-02 14:56:46 +0600

    for loop

---
+++ for.js
@@ -0,0 +1,40 @@
+///// While LOOP;
+
+    let  number = 1;
+    while (number <= 10 ) {
+        console.log(number);
+        number++;
+    }
+
+
+    //FOR LOOP
+
+    for ( let num = 0; num <= 10; num++ ){
+        console.log('Original Number is', num);
+    }
+
+    for (let count = 5; count <=50 ; count++)
+        console.log('count number is:', count);
+
+    for(let i = 0; i <= 10 ; i++){
+        console.log(i);
+    }
+
+    for ( let x = 0; x <= 50 ; x++ ) {
+        console.log(x);
+    }
+
+
+    // loop even number function
+
+    for( i = 0; i <= 50 ; i = i + 2) {
+        console.log('Thiu is serial of even number' , i);
+    }
+
+    //loop odd number
+
+    for( let z = 1; z <= 30; z = z + 2){
+        console.log('This is odd Number:', z);
+    }
+
+    
