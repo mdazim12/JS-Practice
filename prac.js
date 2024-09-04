23bb0915c65cba1be47ca35c63c49c6f4fb29a6c
Azim <azimuddin807@gmail.com>
2024-09-05 00:38:43 +0600

    push & pop

---
+++ prac.js
@@ -0,0 +1,24 @@
+// const numbers = [34,33,53,45,24];
+                 
+// // console.log(numbers.length)
+// // const x = numbers[2]
+// // console.log(x)
+// console.log(numbers)
+// // numbers.push(100)
+// // numbers.pop()
+// // numbers.pop()
+// const x = numbers.pop()
+
+
+// const numbers = [34, 33,354,34,454,45,455,6];
+// const x = numbers.length;
+// // console.log(x);
+// const addItem = 14;
+// const y = numbers.push(100);
+// console.log(numbers)
+
+const friends = ['karim', 'Rohim', 'jaber', 'ahad'];
+// console.log(typeof friends);
+friends.push('mission','jjjj','uuuu')
+friends.pop();
+console.log(friends)
