9d6b1536e4cf7b33963870c615330570aa734677
Azim <azimuddin807@gmail.com>
2024-09-05 01:00:22 +0600

    shift & unshift

--- prac.js
+++ prac.js
@@ -18,7 +18,31 @@
 // console.log(numbers)
 
 const friends = ['karim', 'Rohim', 'jaber', 'ahad'];
-// console.log(typeof friends);
-friends.push('mission','jjjj','uuuu')
-friends.pop();
-console.log(friends)
+// // console.log(typeof friends);
+// friends.push('mission','jjjj','uuuu')
+// friends.pop();
+// friends.pop();
+// friends.pop();
+// console.log(friends)
+
+const shift = friends.shift();
+const shifts = friends.shift();
+const shiftss = friends.shift();
+
+const unshift = friends.unshift('Hello whatsapp','I am okay');
+const unshifts = friends.unshift('i am new here')
+const unshiftss = friends.unshift('who tell you are frist');
+console.log(friends);
+
+
+
+
+const friuts = ['amm','jamm','lichu','kola'];
+const number = [2,4,6,7,2,56];
+const flower = ['beli','shapla','joba','golap'];
+
+// console.log(friuts);
+// console.log(number);
+// console.log(flower);
+
+
