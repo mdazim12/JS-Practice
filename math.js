d72c4f65c2061f08e3df8eec4ae84b6d43af630c
Azim <azimuddin807@gmail.com>
2024-09-07 00:37:27 +0600

    random number

---
+++ math.js
@@ -0,0 +1,19 @@
+const min = Math.min(45,345,4,324,432,5,45,6,6,67,2,45,3,5432,5,432);
+console.log('Minimun Number is:',min)
+
+const max = Math.max(3,43,24,3,245,31,5,3,4,324,3,5,3,24,24,32,5);
+console.log('Max Number is:',max)
+
+console.log('This is PL Number:',Math.PI)
+
+console.log('This is round Number',Math.round(1.7))
+
+console.log('This is floor Number',Math.floor(4.8));
+
+console.log('This is Celing Number',Math.ceil(4.9));
+
+console.log(Math.random()*100);
+
+// Random number with full number
+const randomNumber = Math.random() * 100;
+console.log('This is Full random number:',Math.round(randomNumber));
