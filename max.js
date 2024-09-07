406d1e40f8f25590b9e47793ba1c552f567f2526
Azim <azimuddin807@gmail.com>
2024-09-07 20:42:22 +0600

    max number

---
+++ max.js
@@ -0,0 +1,21 @@
+const disha = 56;
+const salman = 456;
+
+if(disha <  salman ){
+    console.log('salman get stavery');
+}
+else{
+    console.log('Diha Will be stravery')
+}
+
+ function getMax(num1, num2){
+    if(num1 > num2){
+        return num1;
+    }
+    else{
+        return num2;
+    }
+ }
+
+ const max = getMax(299,23);
+ console.log('Max Number is:',max);
