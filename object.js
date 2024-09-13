94357c789bddc4ad627d8c7f14daf0b1131e1ecf
Azim <azimuddin807@gmail.com>
2024-09-13 20:57:16 +0600

    start module 24

---
+++ object.js
@@ -0,0 +1,13 @@
+const student = {
+    name : 'Sakib Khan',
+    id : 121,
+    adress : 'Movie center',
+    isSingle : false,
+    friends : ['apu' , 'Bublie','Pornima','Razz'],
+    car : {
+        brand : 'Tesla',
+        price : 200000,
+        model : 2034,
+    }
+}
+console.log(student.car.model);
