0db53b5c0929660ec0bb81172e833423714c6576
Azim <azimuddin807@gmail.com>
2024-09-18 22:37:50 +0600

    new input button add

--- Module-27/part-1/js/home.js
+++ Module-27/part-1/js/home.js
@@ -1,1 +1,8 @@
-console.log('home added')
+
+    document.getElementById('btn-money-add').addEventListener('click',function(event){
+        event.preventDefault();
+        
+        const addMoneyInput = document.getElementById('input-money-add').value;
+        const inputPinNumber = document.getElementById('input-pin-number').value;
+        console.log(addMoneyInput , inputPinNumber)
+    })
