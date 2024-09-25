45f2754d78f90be6e4ba5ab2071afd518bb0e55e
Azim <azimuddin807@gmail.com>
2024-09-25 23:46:46 +0600

    Distructing

---
+++ Module-30/destructure.js
@@ -0,0 +1,26 @@
+
+    const actor = {
+        name : 'Ananta Jolil',
+        age : 30,
+        phone : '018930939009',
+        money : 2000000,
+    }
+
+    // const name = actor.age
+
+    // console.log(name)
+    // console.log(name)
+    // console.log(name)
+    // console.log(name)
+    // console.log(name)
+
+    // const {phone, age} = actor;
+   //  console.log(phone, age )
+
+    const {phone , age : boish} = actor;
+    console.log(phone, boish)
+
+
+    //Distructure form an arrray
+    
+   
