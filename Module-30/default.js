b249bcce029c65d31223ef1c52be474756afbe12
Azim <azimuddin807@gmail.com>
2024-09-25 21:38:39 +0600

    function perameter set a default value

--- Module-30/default.js
+++ Module-30/default.js
@@ -1,10 +1,32 @@
 
-function add(num1, num2) {
+function add(num1, num2 = 5) {
     const result = num1 + num2;
-
+    console.log(result, num1, num2)
     return result;
 }
 
-const sum = add(5, 7);
+const sum = add(5, 20);
 
-console.log(sum)
+function fullName(first , last = 'ABul'){
+    const full = first + ' ' + last;
+    console.log(full)
+
+}
+fullName('kotim' )
+
+
+
+// Array expect as a function argument
+
+function friend(num = []){
+
+}
+
+// Object expect as a function argument
+
+    function person(human = {}){
+
+    }
+
+
+
