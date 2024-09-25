ccb6b65a3a70b12bb0221ab82a0e2ed24a4bd0b0
Azim <azimuddin807@gmail.com>
2024-09-26 01:06:33 +0600

    module done

---
+++ Module-30/looping.js
@@ -0,0 +1,11 @@
+
+    const numbers = [1, 4, 5, 7, 9];
+
+    for (const num of numbers){
+        // console.log(num)
+    }
+
+    const nobab = 'Siraj ul dullah';
+     for(const char of nobab){
+        console.log(char)
+     }
