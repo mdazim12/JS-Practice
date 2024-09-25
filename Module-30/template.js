10b5775904fb79e0cb63f6340bbf99eaf0a1e2d5
Azim <azimuddin807@gmail.com>
2024-09-25 22:20:15 +0600

    template for dynamic

---
+++ Module-30/template.js
@@ -0,0 +1,13 @@
+const first = 'jann';
+const last = 'phaki';
+const great = 'potash potash'
+
+const name = first + " " +  last + " " + great;
+
+// console.log(name)
+
+const a = 20; 
+const b = 10;
+const math = `The ultimate Number is ${a} and secound number is ${b} Total is ${a + b} `;
+
+console.log(math)
