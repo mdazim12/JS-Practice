46cd12c53b08a3ae1a9e144813919ba977ed2eca
Azim <azimuddin807@gmail.com>
2024-09-25 23:19:18 +0600

    spread oparator

---
+++ Module-30/spread.js
@@ -0,0 +1,15 @@
+
+    const max = Math.max(3,35,3,45,45,4,6,4,325,4,56,3,4325);
+    const numbers = [24, 34, 36, 43, 63, 45, 99];
+    const maxArray = Math.max(...numbers)
+
+    console.log(...numbers)
+    console.log(maxArray)
+
+
+    // use sprad oparator to copy
+    const friend = [2,3,4,3,7,5];
+    const dosto = friend;
+    dosto.push(12);
+    console.log(dosto)
+    console.log(friend);
