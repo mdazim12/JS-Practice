348d53450a7af99c02293b8dbf2212d4c90544cd
Azim <azimuddin807@gmail.com>
2024-09-25 22:39:33 +0600

    arrow function

---
+++ Module-30/arrow.js
@@ -0,0 +1,29 @@
+
+    // function add(a, b){
+    //     const result = a + b ;
+    //     return result;
+    // }
+
+    // function add(a, b){
+      
+    //     return a + b;
+    // }
+
+    // const sum = add(5, 90);
+
+    // console.log(sum)
+
+
+   // function expression
+
+    const add2 =  function(a, b) {
+        return a + b;
+    }
+    add2(6 , 8)
+ 
+
+    //arrow function
+
+    const add3 = (a, b) => a + b;
+    const sum = add3(10 , 20)
+    console.log(`The number is ${sum}`)
