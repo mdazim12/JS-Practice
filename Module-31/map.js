f470e82565dfffbd8ec4189bdc86c7f23bd74af4
Azim <azimuddin807@gmail.com>
2024-09-26 22:28:07 +0600

    map without any function

--- Module-31/map.js
+++ Module-31/map.js
@@ -1,10 +1,28 @@
 
+    // const numbers = [2,4,6,8,10];
+
+    // const dobuled = [];
+
+    // for(num of numbers){
+    //     const dobule = num * 2;
+    //     dobuled.push(dobule)
+    // }
+    // console.log(dobuled)
+
     const numbers = [2,4,6,8,10];
 
-    const dobuled = [];
-
-    for(num of numbers){
-        const dobule = num * 2;
-        dobuled.push(dobule)
+    function dobuleIt(num){
+        // console.log('Num now :', num)
+        return num * 2;
     }
-    console.log(dobuled)
+    const result = numbers.map(dobuleIt)
+
+    // console.log('This is form map',result)
+
+
+    const dobule2 = n => n * 2;
+    const output = numbers.map(dobule2);
+    // console.log(output)
+
+    const output2 = numbers.map( n => n * 2)
+    console.log(output2)
