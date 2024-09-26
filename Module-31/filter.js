06ba9c199bf90a20a78b8a07b1b3b11a7b3bbf95
Azim <azimuddin807@gmail.com>
2024-09-26 23:00:48 +0600

    map filter

---
+++ Module-31/filter.js
@@ -0,0 +1,9 @@
+
+const numbers = [1, 5, 6, 4, 15];
+const player = [50,  45, 68, 85, 54, 74, 64, 56]
+
+// const selected = player.filter(p => p > 60)
+const selected2 = player.filter(p => p > 90)
+
+const oddNumebr = player.filter(num => num % 2 === 0)
+console.log(oddNumebr)
