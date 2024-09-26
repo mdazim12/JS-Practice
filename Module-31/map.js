730f7764f23d188f716d2b200b929c8cfe351130
Azim <azimuddin807@gmail.com>
2024-09-26 22:14:10 +0600

    for of an array

---
+++ Module-31/map.js
@@ -0,0 +1,10 @@
+
+    const numbers = [2,4,6,8,10];
+
+    const dobuled = [];
+
+    for(num of numbers){
+        const dobule = num * 2;
+        dobuled.push(dobule)
+    }
+    console.log(dobuled)
