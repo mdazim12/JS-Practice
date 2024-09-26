45e0948a09a32afae428ef18bbc0360277c20fa6
Azim <azimuddin807@gmail.com>
2024-09-26 22:39:25 +0600

    array map loop

---
+++ Module-31/more-map.js
@@ -0,0 +1,16 @@
+
+    const numbers = [12 ,10,15 ,7]
+
+    const dobule = numbers.map( n => n * 2)
+    // console.log(dobule)
+    const fiveBouns = numbers.map( n => n + 5)
+    // console.log(fiveBouns)
+    
+    const half = numbers.map(num => num / 2)
+    // console.log(half)
+
+    const friends = ['Tom' ,'Jerry', 'Michel', 'Oliveya'];
+    const length = friends.map( friends => friends.length )
+    console.log(length)
+    const fristLatter = friends.map( friends => friends[0])
+    console.log(fristLatter)
