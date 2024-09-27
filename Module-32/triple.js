ee12b153ae2aab41097ef9bf1579931c025e47ee
Azim <azimuddin807@gmail.com>
2024-09-27 23:05:46 +0600

    dobule quall & triple quall

---
+++ Module-32/triple.js
@@ -0,0 +1,10 @@
+
+    const frist = 0;
+    const secound = '0';
+
+    if( frist === secound ){
+        console.log('Value is equall')
+    }
+    else{
+        console.log('Value is not equall')
+    }
