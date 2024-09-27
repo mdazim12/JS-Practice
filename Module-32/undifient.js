0c4c2efdb1ac75328366bd76ed9febb392e06c97
Azim <azimuddin807@gmail.com>
2024-09-27 22:34:09 +0600

    undident type

---
+++ Module-32/undifient.js
@@ -0,0 +1,36 @@
+
+    let frist ;
+    // console.log(frist)
+
+    function secound(a, b){
+        // const total = a + b;
+    }
+    const result = secound()
+    // console.log(result)
+
+    function third(a, b,c, d){
+        const total = a + b + c + d ;
+        // console.log(total)
+    }
+    third(2, 4, 5)
+
+    function noNegitive(a, b){
+        if( a < 0 || b < 0){
+            return
+        }
+        return a + b
+    }
+    const total = noNegitive(2, 5)
+    //console.log(total)
+
+
+    const fifth = {id : 1, name : 'korim', age : 50}
+    // console.log(fifth.id , fifth.name, fifth.age , fifth.salay)
+
+    const six = [2, 43, 65, 76, 48]
+    //console.log(six[1], six[4], six[67], six[3])
+
+    const eight = undefined;
+
+    const nine = null
+    console.log(typeof null)
