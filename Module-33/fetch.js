1d2fa595176d9a1d9b5952c17cb67db32e434505
Azim <azimuddin807@gmail.com>
2024-09-30 22:29:39 +0600

    fetch data & callback function

--- Module-33/fetch.js
+++ Module-33/fetch.js
@@ -2,15 +2,12 @@
 //       .then(response => response.json())
 //       .then(json => console.log(json))
 
-// const url = 'https://jsonplaceholder.typicode.com/todos/1'
-// fetch(url)
-// .then(response => response.json())
-// .then(json => console.log(json))
 
-    function fetchData() {
-        const url = 'https://jsonplaceholder.typicode.com/todos/1'
-        fetch(url)
-            .then(response => response.json())
-            .then(json => console.log(json))
-    }
+
+function fetchData() {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1'
+    fetch(url)
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
 
