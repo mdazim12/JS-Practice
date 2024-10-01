6dc5a34733ad0e10dd213923db0b28b468b2a5bf
Azim <azimuddin807@gmail.com>
2024-10-01 23:05:06 +0600

    interval

--- Module-34/interval.js
+++ Module-34/interval.js
@@ -3,11 +3,21 @@
 console.log(1)
 console.log(2)
 console.log(3)
-setTimeout(()=> {
-    console.log(3)
-})
 
-setTimeout(() => {
-    console.log ('tui aktu pore ai')
-} , 4000)
+// setTimeout(()=> {
+//     console.log(3)
+// })
 
+// setTimeout(() => {
+//     console.log ('tui aktu pore ai')
+// } , 4000)
+
+
+let num = 0;
+
+const clockId = setInterval( () => {
+    num++;
+    console.log( clockId,num)
+}, 2000)
+console.log(4)
+console.log(5)
