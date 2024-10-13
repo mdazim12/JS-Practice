344ec9746d189aeb0d707748a41538dfb783637c
Azim <azimuddin807@gmail.com>
2024-10-13 21:34:33 +0600

    display product

--- Module 35/js/app.js
+++ Module 35/js/app.js
@@ -2,10 +2,32 @@
     // const hello = localStorage.getItem('hello')
     // console.log(hello)
 
-    const setAge = () =>{
-        localStorage.setItem('age' , '45')
-    }
+    // const setAge = () =>{
+    //     localStorage.setItem('age' , '45')
+    // }
 
-    const removeAge = () => {
-        localStorage.removeItem('age')
-    }
+    // const removeAge = () => {
+    //     localStorage.removeItem('age')
+
+    
+
+const addProduct = () =>{
+    const productName = document.getElementById('product-name');
+    const productQuaitiy = document.getElementById('product-quaintity');
+    const productFeild = productName.value;
+    const quaintityFeild = productQuaitiy.value;
+    productName.value = '';
+    productQuaitiy.value =''
+
+
+    // console.log(productFeild , quaintityFeild)
+    displayProduct(productFeild, quaintityFeild);
+}
+
+
+const displayProduct = (productFeild, quaintityFeild) => {
+    const productContainer = document.getElementById('product-container');
+    const li = document.createElement('li');
+    li.innerText =  `${productFeild} ${quaintityFeild}`
+    productContainer.appendChild(li)
+}
