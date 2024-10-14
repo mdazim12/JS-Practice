8d52a5e818bc89d5e3621e96c594c902d583381c
Azim <azimuddin807@gmail.com>
2024-10-14 21:06:05 +0600

    map

---
+++ Module 37/array-method.js
@@ -0,0 +1,12 @@
+
+    const products = [
+        { name : 'laptop' , price: 32000, brand : 'HP' , color : 'gray' },
+        { name : 'Phone' , price: 2000, brand : 'samsung' , color : 'black' },
+        { name : 'wacth' , price: 400, brand : 'casio' , color : 'red' },
+        { name : 'sunclass' , price: 600, brand : 'rebon' , color : 'yellow' },
+        { name : 'laptop' , price: 32000, brand : 'HP' , color : 'gray' },
+        { name : 'camera' , price: 9000, brand : 'canon' , color : 'sliver' },
+    ]
+
+    const brands = products.map(product => product.brand);
+    console.log(brands)
