848b00d15bd281bf4093fedd11b3a107dd0a099f
Azim <azimuddin807@gmail.com>
2024-10-16 00:36:36 +0600

    turnary conditional logic

--- Module 38/explore-react-app/src/Todo.jsx
+++ Module 38/explore-react-app/src/Todo.jsx
@@ -6,11 +6,30 @@
 //     }
 
 
- export default function todo({task, isDone}){
-        if(isDone){
-            return <li>Finised : {task}</li>
-        }
-        else{
-            return <li>Work on : {task} </li>
-        }
+//  export default function todo({task, isDone}){
+//         if(isDone){
+//             return <li>Finised : {task}</li>
+//         }
+//         else{
+//             return <li>Work on : {task} </li>
+//         }
+//     }
+
+    // export default function todo({task, isDone}){
+    //             return (
+    //                 <li> {isDone ? 'Finised the work' : 'Working the labor'} {task} </li>
+    //             )
+    //         }
+        
+    // export default function todo({task, isDone}){
+    //             return (
+    //                 <li> {task} {isDone && ':Done'} </li>
+    //             )
+    //         }
+
+    export default function todo ({task , isDone}){
+        return (
+            <li> {task} {isDone || ':Do it right now'} </li>
+        )
     }
+        
