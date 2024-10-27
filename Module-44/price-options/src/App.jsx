43ca94553a5d6a0ffb2343a5c77dbfef950e9990
Azim <azimuddin807@gmail.com>
2024-10-27 22:31:12 +0600

    setup project

---
+++ Module-44/price-options/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1>Vite + React default</h1>
+      
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
