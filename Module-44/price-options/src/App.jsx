0bd8a07a9e25a34ad21cdf3416a94e5d734f45d9
Azim <azimuddin807@gmail.com>
2024-10-28 18:45:28 +0600

    Line chart

--- Module-44/price-options/src/App.jsx
+++ Module-44/price-options/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import LimeChart from './components/LimeChart/LimeChart'
 // import DaisyNav from './components/DaisyNav/DaisyNav'
 import Navbar from './components/Navbar/Navbar'
 import PriceOptions from './components/PriceOptions/PriceOptions'
@@ -13,6 +14,7 @@ function App() {
      {/* <DaisyNav></DaisyNav> */}
      <Navbar></Navbar>
       <PriceOptions></PriceOptions>
+      <LimeChart></LimeChart>
      
     
 
