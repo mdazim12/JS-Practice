05cd333693b64a0ec5c0d2f9bdc012e3fcb5f3a1
Azim <azimuddin807@gmail.com>
2024-10-29 15:12:05 +0600

    useLocation

--- Module-45/react-router/src/components/Header/Header.jsx
+++ Module-45/react-router/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import {  NavLink } from 'react-router-dom';
 import './Header.css'
 
 
@@ -6,11 +6,13 @@ const Header = () => {
     return (
         <nav>
             
-            <Link to = "/">Home</Link>
-            <Link to = "/about">About</Link>
-            <Link to = "/contact">Contact</Link>
-            <Link to = "users">Users</Link>
-            <Link to = "/posts">Posts</Link>
+            
+
+            <NavLink to = "/">Home</NavLink>
+            <NavLink to = "/about">About</NavLink>
+            <NavLink to = "/contact">Contact</NavLink>
+            <NavLink to = "/users">Users</NavLink>
+            <NavLink to = "/posts">Posts</NavLink>
        
 
 
--- Module-45/react-router/src/components/Home/Home.jsx
+++ Module-45/react-router/src/components/Home/Home.jsx
@@ -1,14 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
+
+    const navigation = useNavigation()
+    const loacation = useLocation()
+    console.log(loacation)
+
     return (
         <div className="home">
             <Header></Header>
-           <h1> This is main home</h1>
-           <p>Best application over the world by me</p>
-            <Outlet></Outlet>
+            {
+                navigation.state === "loading" ?
+                <p className="red">Loading</p> : <Outlet></Outlet>
+            }
+           
+            
             <Footer></Footer>
         </div>
     );
