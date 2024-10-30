a8f5e0e5eff3f46ffc200103b678e32fabcca1a8
Azim <azimuddin807@gmail.com>
2024-10-30 22:21:31 +0600

    password validcation

--- Module-46/from-master/src/components/StatefullForm/StatefullForm.jsx
+++ Module-46/from-master/src/components/StatefullForm/StatefullForm.jsx
@@ -6,10 +6,19 @@ const StatefullForm = () => {
     const [name,setName] = useState(null)
     const [email,setEmail] = useState(null);
     const [password, setPassword] = useState(null)
-
+    const [error,setError] = useState('')
+ 
     const handleSubmit = e => {
         e.preventDefault();
-        console.log(name,email,password)
+
+        if(password.length < 5){
+            setError('password must be 6 charahtar')
+        }
+        else{
+            setError('')
+            console.log(name,email,password)
+        }
+       
     }
 
     const handleEmailChange = e => {
@@ -36,6 +45,9 @@ const StatefullForm = () => {
                     <br />
                     <input onChange={handlePassword} type="password" name="password" />
                     <input type="submit" value="Sumnit" />
+                    {
+                        error && <p>{error}</p>
+                    }
                 </form>
             </div>
         </div>
