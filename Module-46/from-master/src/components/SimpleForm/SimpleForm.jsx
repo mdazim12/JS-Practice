af18b124426c86f9f2626ea70762051d38ac1f36
Azim <azimuddin807@gmail.com>
2024-10-30 21:41:12 +0600

    Handle form with onSubmi

--- Module-46/from-master/src/components/SimpleForm/SimpleForm.jsx
+++ Module-46/from-master/src/components/SimpleForm/SimpleForm.jsx
@@ -1,10 +1,20 @@
 
 
 const SimpleForm = () => {
+
+    const handleSubmit = e =>{
+        e.preventDefault()
+        console.log(e.target.name.value);
+        console.log(e.target.email.value)
+        
+    }
+
     return (
-        <div>
+        <div onSubmit={handleSubmit}>
             <form>
-                <input type="text" />
+                <input type="text" name="name" />
+                <br />
+                <input type="email" name="email" />
                 <input type="submit" value="Sumnit" />
             </form>
         </div>
