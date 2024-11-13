f4427315512104baf721dcdb860615be460c5a9f
Azim <azimuddin807@gmail.com>
2024-11-13 16:34:32 +0600

    setup

--- Module-46/from-master/src/App.jsx
+++ Module-46/from-master/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   return (
     <>
       
-      <h1>Vite + React</h1>
+      <h1> Form Master</h1>
       
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefullForm></StatefullForm> */}
