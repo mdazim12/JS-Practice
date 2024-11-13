3c940a759b8c23a7f266dcf0f92b865afb6687e2
Azim <azimuddin807@gmail.com>
2024-11-13 17:00:41 +0600

    firebase setup

---
+++ Module-49/simple-firebase/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h2>This is Header so respect him</h2> 
+        </div>
+    );
+};
+
+export default Header;
--- Module-49/simple-firebase/src/main.jsx
+++ Module-49/simple-firebase/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import Main from './components/Layouts/Main';
+import Home from './components/Home/Home';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [{
+      path : '/',
+      element : <Home></Home>
+    },
+  
+  ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </StrictMode>,
 )
