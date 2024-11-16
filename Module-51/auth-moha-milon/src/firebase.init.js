4e3abbed5b0248f5c21bed347610402f34bcce9a
Azim <azimuddin807@gmail.com>
2024-11-16 18:34:26 +0600

    project setup

---
+++ Module-51/auth-moha-milon/src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCULYiQ9S5oHEVBrCjMYHDG0jYolqsIAhw",
+  authDomain: "auth-moha-milon-942c9.firebaseapp.com",
+  projectId: "auth-moha-milon-942c9",
+  storageBucket: "auth-moha-milon-942c9.firebasestorage.app",
+  messagingSenderId: "982397282091",
+  appId: "1:982397282091:web:fef8b68b56a05364ccc76e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
