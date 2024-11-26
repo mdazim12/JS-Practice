e38c7d97c78a892efa43baaa04345314bb9365c8
Azim <azimuddin807@gmail.com>
2024-11-27 00:09:00 +0600

    get data using id

---
+++ Module-53/my-phone-list/index.js
@@ -0,0 +1,28 @@
+ const express = require('express')
+ const mounts = require('./moutain.json')
+ const app = express();
+ const port = 5000;
+
+ app.get('/',(res,req) => {
+    req.send('This is my main server ki khobor batija')
+ });
+
+ app.get('/mounts',(res,req) => {
+    req.send(mounts)
+ });
+
+ 
+
+
+app.get('/mounts/:id', (req, res) => { // Corrected parameter order
+    const id = parseInt(req.params.id);
+    const mount = mounts.find(mount => mount.id === id) || {};
+    res.send(mount);
+});
+
+ 
+
+
+ app.listen(port, () => {
+    console.log(`my server is running on ${port}`);
+ })
