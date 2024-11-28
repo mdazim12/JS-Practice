a70d5d18e053836ed391d386e99fdacec4501afe
Azim <azimuddin807@gmail.com>
2024-11-28 18:40:44 +0600

    new setup done

---
+++ Module-54/user-management-server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+const users = [
+    {id:1,name:'Sabana',email:'sabana@gmail.com'},
+    {id:2,name:'Sabnnor',email:'sabnoor@gmail.com'},
+    {id:1,name:'Mousumi',email:'mousumi@gmail.com'}
+]
+
+app.get('/', (req,res) => {
+    res.send('user management server is runing Sir')
+})
+
+app.get('/users', (req,res) => {
+    res.send(users)
+})
+
+app.listen(port, () => {
+    console.log(`The server is runing on PORT: ${port}`)
+})
