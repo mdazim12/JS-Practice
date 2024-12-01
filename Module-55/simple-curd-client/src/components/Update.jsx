6ae25d02117e2aaa6cfc07f3387921c3efa470a3
Azim <azimuddin807@gmail.com>
2024-12-01 11:49:22 +0600

    edit user imformation

--- Module-55/simple-curd-client/src/components/Update.jsx
+++ Module-55/simple-curd-client/src/components/Update.jsx
@@ -2,27 +2,46 @@ import { useLoaderData } from "react-router-dom";
 
 
 const Update = () => {
-    const loadUser = useLoaderData();
-    console.log(loadUser);
+    const loadedUser = useLoaderData();
+    console.log(loadedUser);
 
     const handleUpdate = e => {
         e.preventDefault();
         const form = e.target;
         const name = form.name.value;
         const email = form.email.value;
+        const updateUser = {name,email}
         console.log(name,email)
+
+
+        fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+            method : 'PUT',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(updateUser)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount > 0) {
+                alert('user update succesfully')
+            }
+        })
+
+
     }
 
 
     return (
         <div>
-            <h3>Update information of { loadUser.name }</h3>
+            <h3>Update information of { loadedUser.name }</h3>
 
             <form onSubmit={handleUpdate}>
 
-                <input type="text" name="name" id="" defaultValue={loadUser?.name} />
+                <input type="text" name="name" id="" defaultValue={loadedUser?.name} />
                 <br />
-                <input type="email" name="email" id="" defaultValue={loadUser?.email} />
+                <input type="email" name="email" id="" defaultValue={loadedUser?.email} />
                 <br />
                 <input type="submit" value="Update User" />
 
