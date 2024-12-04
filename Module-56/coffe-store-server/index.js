7f42ff5252114576ee99888be1fdac02e0ff0e1b
Azim <azimuddin807@gmail.com>
2024-12-04 14:12:47 +0600

    delete coffe

--- Module-56/coffe-store-server/index.js
+++ Module-56/coffe-store-server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 //middleware use here
@@ -37,6 +37,13 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result)
     } )
+
+    app.get('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.findOne(query)
+      res.send(result);
+    })
     
     app.post('/coffee', async(req,res) => {
       const newCoffe = req.body;
@@ -45,9 +52,16 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/coffee/:id', async (req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result)
+    })
 
 
 
+ 
 
 
 
