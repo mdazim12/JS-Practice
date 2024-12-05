47ac594b4beb932f47c50b91429877a40395bfcb
Azim <azimuddin807@gmail.com>
2024-12-05 21:55:07 +0600

    user delete

--- Module-57/coffee-store-server/index.js
+++ Module-57/coffee-store-server/index.js
@@ -34,6 +34,8 @@ async function run() {
         const database = client.db('coffeeDB');
         const coffeeCollection = database.collection('coffee');
 
+        const userCollection = client.db('coffeeDB').collection('users')
+
 
         app.get('/coffee', async (req, res) => {
             const cursor = coffeeCollection.find();
@@ -75,6 +77,30 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const result = await coffeeCollection.deleteOne(query);
             res.send(result);
+        })
+
+
+        //Users related API
+
+        app.post('/users' , async(req,res) => {
+            const newUser = req.body;
+            console.log('creating new users' , newUser)
+            const result = await userCollection.insertOne(newUser)
+            res.send(result)
+        })
+
+
+        app.get('/users' ,async (req,res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.delete('/users/:id', async (req,res) => {
+            const id = req.params.id;
+            const query = {_id : new  ObjectId(id)}
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
         })
 
 
